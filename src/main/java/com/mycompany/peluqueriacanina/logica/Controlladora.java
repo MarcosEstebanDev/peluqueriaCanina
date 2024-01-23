15e5bf6d9086931c3c4eac00689b6de062c55b05
marcosestebandev <marcosestebandev@gmail.com>
2024-01-23 13:43:01 -0300

    ADD CONTROOLLERS LOGICA AND PERSISTENCE

--- src/main/java/com/mycompany/peluqueriacanina/logica/Controlladora.java
+++ src/main/java/com/mycompany/peluqueriacanina/logica/Controlladora.java
@@ -1,7 +1,9 @@
-
 package com.mycompany.peluqueriacanina.logica;
+import com.mycompany.peluqueriacanina.persistence.ControladoraPersistencia;
 
 
 public class Controlladora {
+    
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
     
 }
