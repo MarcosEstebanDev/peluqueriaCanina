63cabdb6ceedbfabe4396a5411ad9d0da8f5637d
marcosestebandev <marcosestebandev@gmail.com>
2024-01-25 21:34:48 -0300

    Add controladora persistencia

--- src/main/java/com/mycompany/peluqueriacanina/persistence/ControladoraPersistencia.java
+++ src/main/java/com/mycompany/peluqueriacanina/persistence/ControladoraPersistencia.java
@@ -1,6 +1,15 @@
 package com.mycompany.peluqueriacanina.persistence;
 
+import com.mycompany.peluqueriacanina.logica.Duenio;
+import com.mycompany.peluqueriacanina.logica.Mascota;
+
 
 public class ControladoraPersistencia {
-    
+    DuenioJpaController duenioJpa = new DuenioJpaController();
+    MascotaJpaController mascotaJpa = new MascotaJpaController();
+
+    public void guardar(Duenio duenio, Mascota masco) {
+        duenioJpa.create(duenio);
+        mascotaJpa.create( masco);
+    }
 }
